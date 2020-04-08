9aa1060e10c11eba6d2d877d4b3752fd238f38be
elliot771 <62021481+elliot771@users.noreply.github.com>
2020-04-08 16:19:57 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/tuto/')
+def home():
+	return render_template('le_tuto_entier.html')
+
+
+@app.route('/contact/')
+def contact():
+	return render_template('contact.html')
+
+@app.route('/vocabulaire')
+def vocabulaire():
+	return render_template('vocabulaire.html')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+#@app.errorhandler(404)
+#def page_not_found(erreur_404):
+#    return render_template('erreur_404.html'), 404
